e3dda6e1c8441813a6ee329e8929505b6c9f15ff
Ahmed Esmat <112887051+Zeus62@users.noreply.github.com>
2025-02-05 23:12:15 +0200

    database.py

--- src/database.py
+++ src/database.py
@@ -61,7 +61,7 @@ class Database:
                 orders_text.insert(END, f"{item[0]:<15}{item[1]:<10}{item[2]:<10.2f}{item[3]:<10.2f}\n")
                 total_bill += item[3]
 
-            # Display total
+            # Display Total
             orders_text.insert(END, "-" * 45 + "\n")
             orders_text.insert(END, f"{'Total Bill:':<35}${total_bill:.2f}\n")
             orders_text.insert(END, "\n" + "Thank you for visiting us!" + "\n\n")
